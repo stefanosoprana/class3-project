76cbd262bb91bbfdf7d7c91d473c67c7e82e7a98
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-03-18 21:09:41 +0100

    Modified routes and controllers Add sass partials

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,9 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->namespace('API')->namespace('API')->prefix('API')->name('API.')->group(function() {
+    Route::get('/messages', 'MessageController@index')->name('message.index');
     Route::get('/message/{id}', 'MessageController@show')->name('message.show');
-    Route::post('/message/create', 'MessageController@create')->name('message.create');
+    Route::get('/message', 'MessageController@create')->name('message.create');
+    Route::post('/message', 'MessageController@store')->name('message.store');
     Route::post('/apartments/dashboard', 'ApartmentController@create')->name('apartment.dashboard');
 });
--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,11 @@ Route::get('/apartment/{id}', 'ApartmentController@show');
 //tutte le altre rotte appartamenti
 Route::middleware('auth')->group(function () {
     Route::get('/apartments/{user}', 'ApartmentController@userIndex')->name('apartments.user.index');
-    Route::get('/apartment/{id}/statistics', 'ApartmentController@statistics')->name('apartment.statistics');
-    Route::post('/apartment/create', 'ApartmentController@create')->name('apartment.create');
+    Route::get('/apartment', 'ApartmentController@create')->name('apartment.create');
+    Route::post('/apartment', 'ApartmentController@store')->name('apartment.store');
+    Route::patch('/apartment/{id}/update', 'ApartmentController@update')->name('apartment.update');
+    Route::get('/apartment/{id}/statistics', 'ApartmentController@statistics')->name('apartment.statistic');
     Route::delete('/apartment/{id}/delete', 'ApartmentController@destroy')->name('apartment.destroy');
-    Route::patch('/apartment/{id}/edit', 'ApartmentController@edit')->name('apartment.edit');
 });
 
 
@@ -35,12 +36,14 @@ Auth::routes();
 //controllo middleware in HomeController
 Route::get('/home', 'HomeController@index')->name('profile');
 
-Route::post('/message/create', 'MessageController@create')->name('message.create');
+Route::get('/message', 'MessageController@create')->name('message.create');
+Route::post('/message', 'MessageController@store')->name('message.storep');
 
 //rotte per messaggi
 Route::middleware('auth')->group(function () {
     Route::get('/messages/{user}', 'MessageController@index')->name('messages.index');
-    Route::delete('/messages/{id}/delete', 'MessageController@destroy')->name('messages.destroy');
+    Route::get('/message/{id}', 'MessageController@show')->name('message.show');
+    Route::delete('/message/{id}/delete', 'MessageController@destroy')->name('message.destroy');
 });
 
 //rotte per sponsorships
@@ -54,15 +57,16 @@ Route::middleware('permission:Admin')->namespace('Admin')->prefix('Admin')->name
     Route::get('/home', 'HomeController@index')->name('index');
 
     //utenti
-    Route::get('/users', 'UserController@index')->name('users.index');
-    Route::delete('/user/{id}/delete', 'UserController@delete')->name('users.delete');
-    Route::patch('/user/{id}/edit', 'UserController@edit')->name('users.edit');
+    Route::resource('users', 'UserController');
 
     //appartamenti
     Route::get('/apartments', 'ApartmentController@index')->name('apartments.index');
+    Route::get('/apartment/{id}', 'ApartmentController@show')->name('apartment.show');
+    Route::get('/apartment', 'ApartmentController@create')->name('apartment.create');
+    Route::post('/apartment', 'ApartmentController@store')->name('apartment.store');
+    Route::patch('/apartment/{id}/update', 'ApartmentController@update')->name('apartment.update');
     Route::get('/apartment/{id}/statistics', 'ApartmentController@statistics')->name('apartment.statistic');
     Route::delete('/apartment/{id}/delete', 'ApartmentController@destroy')->name('apartment.destroy');
-    Route::patch('/apartment/{id}/edit', 'ApartmentController@edit')->name('apartment.edit');
     Route::get('/apartmens/sponsorships', 'ApartmentController@sponsorships')->name('apartments.sponsorships');
 });
 
