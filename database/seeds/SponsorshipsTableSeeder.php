d48718aa27bef06aacb61e31ebbb18c2cadc5383
paolapog <paopoggini2@gmail.com>
2019-03-21 10:32:49 +0100

    added calls in databaseSeeder and apartmentService

---
+++ database/seeds/SponsorshipsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+// use Illuminate\Database\Seeder;
+// use Illuminate\Support\Carbon;
+// use App\Sponsorship;
+// use App\Apartment;
+// use App\SponsorshipsType;
+//
+// class SponsorshipsTableSeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+//
+//         $now = Carbon::now();
+//
+//         $data = [
+//             [
+//                 'apartment_id' => 1,
+//                 'sponsorships_type_id' => 1,
+//                 'sponsor_created' => $now,
+//                 'sponsor_expired' => $now->addDay(1)
+//             ],
+//             [
+//                 'apartment_id' => 2,
+//                 'sponsorships_type_id' => 2,
+//                 'sponsor_created' => $now,
+//                 'sponsor_expired' => $now->addDay(2)
+//             ],
+//             [
+//                 'apartment_id' => 3,
+//                 'sponsorships_type_id' => 3,
+//                 'sponsor_created' => $now,
+//                 'sponsor_expired' => $now->addDay(3)
+//             ],
+//
+//
+//
+//         ];
+//
+//
+//
+//
+//     }
+}
