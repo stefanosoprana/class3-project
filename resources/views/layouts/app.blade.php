e07b83976ada8c5f7a05acd644d9fb3a28b352bf
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-03-30 15:59:07 +0100

    Modify app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,83 +25,95 @@
 </head>
 <body>
 
-        @include('partials.header')
-
-        <div class="row">
-            <div class="col-12">
-                @if( Route::currentRouteName() === 'apartments.search')
-                    {{--container for search vue--}}
-                    <div class="search" id="search__result">
-                        {{--Search Form--}}
-                        <form class="control" @submit.prevent="getFormValues">
-                @else
-                    {{--container without id vue--}}
-                    <div class="search">
-                        {{--Search Form with get action--}}
-                        <form class="control" action="{{ route('apartments.search') }}" method="get">
-                        @csrf
-                        @method('GET')
+@include('partials.header')
+@if( Route::currentRouteName() === 'apartments.search')
+    {{--container for search vue--}}
+    <div class="search" id="search__result">
+        @else
+            {{--container without id vue--}}
+            <div class="search">
                 @endif
-                        <div id="address-complete">
-                            <div class="form-group">
-                                <label for="address">Indirizzo</label>
-                                <input type="text" id="address" name="address" class="form-control" placeholder="es. via Plutarco, 31 , Guidonia, RM, Italia" autocomplete="off"  v-model="address">
-                            </div>
-                            <input id="latitude" name="latitude" type="hidden" data-geo="lat" value="">
-                            <input id="longitude" name="longitude" type="hidden"  data-geo="lng" value="">
-                        </div>
-                        <div class="form-group">
-                            <label for="radius">Raggio di ricerca</label>
-                            <input type="number" id="radius" name="radius" placeholder="Inserisci il raggio in metri" v-model="radius">
-                        </div>
-                        <div class="form-group">
-                            <label for="beds">Numero minimo di letti</label>
-                            <input type="number" id="beds" name="beds" placeholder="Inserisci il numero di letti" v-model="beds">
-                        </div>
-                        <div class="form-group">
-                            <label for="rooms">Numero minimo di stanze</label>
-                            <input type="number" id="rooms" name="rooms" placeholder="Inserisci il numero di stanze" v-model="rooms">
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <fieldset id="services">
-                                <legend>Servizi</legend>
-                                @foreach($services as $service)
-                                    <input type="checkbox" name="service" value="{{$service->name}}" class="form-check-input" v-model="services">
-                                    <label class="form-check-label" for="{{$service->name}}">{!! $service->icon !!} {{$service->name}}</label>
-                                @endforeach
-                            </fieldset>
-                        </div>
-                        <div class="form-group">
-                            <button id="button-search">Cerca</button>
-                        </div>
-                    </form>
-                        {{--/Search Form--}}
+                <div class="row">
+                    <div class="col-12">
+                        @if( Route::currentRouteName() === 'apartments.search')
+                            {{--Search Form--}}
+                            <form class="control" @submit.prevent="getFormValues">
+                                @else
+                                    {{--Search Form with get action--}}
+                                    <form class="control" action="{{ route('apartments.search') }}" method="get">
+                                        @csrf
+                                        @method('GET')
+                                        @endif
+                                        <div id="address-complete">
+                                            <div class="form-group">
+                                                <label for="address">Indirizzo</label>
+                                                <input type="text" id="address" name="address" class="form-control" placeholder="es. via Plutarco, 31 , Guidonia, RM, Italia" autocomplete="off"  v-model="address">
+                                            </div>
+                                            <input id="latitude" name="latitude" type="hidden" data-geo="lat" value="">
+                                            <input id="longitude" name="longitude" type="hidden"  data-geo="lng" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="radius">Raggio di ricerca</label>
+                                            <input type="number" id="radius" name="radius" placeholder="Inserisci il raggio in metri" v-model="radius">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="beds">Numero minimo di letti</label>
+                                            <input type="number" id="beds" name="beds" placeholder="Inserisci il numero di letti" v-model="beds">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="rooms">Numero minimo di stanze</label>
+                                            <input type="number" id="rooms" name="rooms" placeholder="Inserisci il numero di stanze" v-model="rooms">
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <fieldset id="services">
+                                                <legend>Servizi</legend>
+                                                @foreach($services as $service)
+                                                    <input type="checkbox" name="service" value="{{$service->name}}" class="form-check-input" v-model="services">
+                                                    <label class="form-check-label" for="{{$service->name}}">{!! $service->icon !!} {{$service->name}}</label>
+                                                @endforeach
+                                            </fieldset>
+                                        </div>
+                                        <div class="form-group">
+                                            <button id="button-search">Cerca</button>
+                                        </div>
+                                    </form>
+                            {{--/Search Form--}}
+                    </div>
+                    {{--/col-12--}}
+                </div>
+                {{--/row--}}
 
-                        {{--Main--}}
-                        <main class="py-4">
-                        @if(session('status'))
-                            <div class="alert mt-5">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-12 alert-warning">
-                                            {{session('status')}}
+                {{--Main--}}
+                <main class="py-4">
+                    {{--Status--}}
+                    @if(session('status'))
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="alert mt-5">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-12 alert-warning">
+                                                {{session('status')}}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endif
-                        @yield('content')
-                    </main>
-                    {{--/Main--}}
+                        </div>
+                    @endif
+                    {{--/Status--}}
 
-                </div>
-                {{-- /container for vue or generic--}}
+                    {{--Content--}}
+                    @yield('content')
+                    {{--/Content--}}
+                </main>
+                {{--/Main--}}
+
+
             </div>
-            {{--/col-12--}}
-        </div>
-        {{--/row--}}
+    {{-- /container for vue or generic--}}
 
-        @include('partials.footer')
+    @include('partials.footer')
 
 </body>
 </html>
