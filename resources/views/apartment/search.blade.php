06c8bf557ffc58c12adb3144f6a84422edb290ae
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-04-04 21:06:40 +0200

    Modified search

--- resources/views/apartment/search.blade.php
+++ resources/views/apartment/search.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.app')
 @section('content')
-    <div class="sponsorships mb-5">
-        <div class="container">
+    <div class="apartments">
+        <div class="apartments__main">
+            <div class="apartments__main__title">
 
-            {{-- Titolo Evidenza --}}
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="title">La tua ricerca</h2>
+            </div>
+            <div class="apartments__main__content">
+              <div class="container">
+
+                {{-- Titolo Evidenza --}}
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="title">La tua ricerca</h2>
+                    </div>
+                </div>
+                {{-- End Titolo Evidenza --}}
+
+                <div class="container mt-5">
+                    <div class="search__results row">
+                        <card v-for="card in apartments" v-bind:card="card" :key="card.name"></card>
+                        <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler" @distance="1" force-use-infinite-wrapper="true"></infinite-loading>
+                    </div>
                 </div>
             </div>
-            {{-- End Titolo Evidenza --}}
-
-            <div class="container mt-5">
-                <div class="search__results row">
-                    <card v-for="card in apartments" v-bind:card="card" :key="card.name"></card>
-                    <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler" @distance="1" force-use-infinite-wrapper="true"></infinite-loading>
-                </div>
             </div>
         </div>
+
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-            <img src="{{asset('img/boolbnb-logo.svg')}}" alt="">
+            <img src="{{asset('img/boolbnb-logo.svg')}}" alt="" class="img-fluid">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
