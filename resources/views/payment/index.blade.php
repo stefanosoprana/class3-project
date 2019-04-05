b167c63e626ebaf3b11c65c1261f784fe274a5bb
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-04-05 08:46:45 +0200

    Add css payments

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Sponsorizza l'appartamento {{$apartment->title}}</h1>
-            </div>
-        </div>
-        <div class="sponsorships">
-            <div class="row">
-                @foreach($sponsorships_type as $sponsorship_type)
-                    <div class="col-4">
-                        <div class="sponsorships__button text-center">
-                            <p>{{$sponsorship_type->name}} a</p>
-                            <p> <a href="{{route('sponsorships.payment', [$apartment->id, $sponsorship_type->id])}}" class="btn btn-primary">{{$sponsorship_type->price}} €</a></p>
+    <div class="pay">
+        <div class="pay__main">
+            <div class="pay__main__title">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1>Sponsorizza {{$apartment->title}}</h1>
                         </div>
                     </div>
-                @endforeach
+                </div>
+            </div>
+            <div class="pay__main__content mt-5">
+                <div class="container">
+                    <div class="sponsorships">
+                        <div class="row">
+                            @foreach($sponsorships_type as $sponsorship_type)
+                                <div class="col-4">
+                                    <div class="sponsorships__button text-center">
+                                        <h4 class="title-blue">{{$sponsorship_type->name}} a</h4>
+                                        <p> <a href="{{route('sponsorships.payment', [$apartment->id, $sponsorship_type->id])}}" class="btn btn-primary">{{$sponsorship_type->price}} €</a></p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/payment/pay.blade.php
+++ resources/views/payment/pay.blade.php
@@ -1,22 +1,35 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div id="alert-dropin">
-
-                </div>
-                <div id="success-payment" hidden>
-                    <a href="{{route('apartments.user.index', Auth::user())}}">Torna agli appartamenti</a>
+    <div class="pay">
+        <div class="pay__main">
+            <div class="pay__main__title">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1>Effetua il pagamento</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div id="dropin-container" data-token="{{$client_token}}"  data-action="{{ route('sponsorships.process', $apartment->id) }}" data-sponsorship="{{$sponsorship}}" data-apartment="{{$apartment->id}}"></div>
-                <button id="submit-button">Request payment method</button>
+        <div class="pay__main__content mt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div id="alert-dropin">
+
+                        </div>
+                        <div id="success-payment" hidden>
+                            <a href="{{route('apartments.user.index', Auth::user())}}">Torna agli appartamenti</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div id="dropin-container" data-token="{{$client_token}}"  data-action="{{ route('sponsorships.process', $apartment->id) }}" data-sponsorship="{{$sponsorship}}" data-apartment="{{$apartment->id}}"></div>
+                        <button id="submit-button">Request payment method</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
 @endsection
