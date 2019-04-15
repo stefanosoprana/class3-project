c559600f82ee266713f12d6d61c0d29143b4913f
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-04-15 11:22:31 +0200

    Add comment

--- resources/views/apartment/statistics.blade.php
+++ resources/views/apartment/statistics.blade.php
@@ -1,53 +1,52 @@
 @extends('layouts.app')
 @section('content')
-  <div class="statistics">
-    <div class="statistics__title">
-      <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h1>STATISTICHE</h1>
-        </div>
-      </div>
-    </div>
-    </div>
-    <div class="statistics__graphic">
-      <div id="charts">
-        <div class="container">
-          <div class="statistics__graphic__show-button">
-            <div class="row">
-                <div class="col-4 offset-8 text-right">
-                    <a href="{{ route('apartment.show', $apartment->id) }}" class="btn btn-default"><i class="fas fa-eye"></i>Visualizza Appartamento</a>
-                </div>
-            </div>
-            </div>
-            <div class="statistics__graphic__selector">
-            <div class="row">
-                <div class="col-12">
-                  <p><b>Seleziona anno</b></p>
-                        <select name="year" id="year" v-model="selected"  @change="onChange()">
-                            @foreach($years as $year)
-                                <option value="{{$year}}" {{($year === $years[count($years) -1]) ? 'selected' : ''}}>{{$year}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-            </div>
-            </div>
-            <div class="statistics__graphic__main">
-            <div class="row">
-                <div class="col-6"  v-for="chart in charts" :id="chart.name">
-                    <div class="chart" >
-                        <chart-component :chart-data="chart.chartdata" v-if="chart.loaded" :options="chart.options" ></chart-component>
+    <div class="statistics">
+        <div class="statistics__title">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>STATISTICHE</h1>
                     </div>
                 </div>
-               {{-- <div class="col-6">
-                    <div class="chart" id="chart-messages">
-                        <chart-component  :chart-data="chartdata" v-if="loaded" :options="options" ref="messages"></chart-component>
-                    </div>
-                </div>--}}
-            </div>
             </div>
         </div>
+        <div class="statistics__graphic">
+            <div id="charts">
+                <div class="container">
+                    <div class="statistics__graphic__show-button">
+                        <div class="row">
+                            <div class="col-4 offset-8 text-right">
+                                <a href="{{ route('apartment.show', $apartment->id) }}" class="btn btn-default"><i class="fas fa-eye"></i>Visualizza Appartamento</a>
+                            </div>
+                        </div>
+                    </div>
+                    {{--Select--}}
+                    <div class="statistics__graphic__selector">
+                        <div class="row">
+                            <div class="col-12">
+                                <p><b>Seleziona anno</b></p>
+                                <select name="year" id="year" v-model="selected"  @change="onChange()">
+                                    @foreach($years as $year)
+                                        <option value="{{$year}}" {{($year === $years[count($years) -1]) ? 'selected' : ''}}>{{$year}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    {{--/Select--}}
+                    {{--Charts--}}
+                    <div class="statistics__graphic__main">
+                        <div class="row">
+                            <div class="col-6"  v-for="chart in charts" :id="chart.name">
+                                <div class="chart" >
+                                    <chart-component :chart-data="chart.chartdata" v-if="chart.loaded" :options="chart.options" ></chart-component>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{--/Charts--}}
+                </div>
+            </div>
         </div>
     </div>
-  </div>
 @endsection
